feeca5c88738fad9594c2063b695f2ccee783a11
statitica <dave@dave-mate>
2018-07-18 21:46:47 +1000

    changed for scanning

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@ echo "Choose a user to install the scripts for"
 echo "or hold CTRL-C to cancel and exit"
 select you in "$(ls /home)"
 do
-  mkdir /home/$you/.easy-actions
+  mkdir -p /home/$you/.easy-actions/scripts
   mkdir -p /home/$you/.local/share/file-manager/actions
-  cp -Rv ./scripts /home/$you/.easy-actions
+  cp -Rv ./scripts/Email /home/$you/.easy-actions/scripts
+  cp -Rv ./scripts/PDF /home/$you/.easy-actions/scripts
+  cp -Rv ./scripts/Printing /home/$you/.easy-actions/scripts
+  cp -Rv ./scripts/Scanning /home/$you/.config/caja/scripts
+  chmod 755 ./scripts/Scanning /home/$you/.config/caja/scripts/Scanning/Scan_Here
+  chmod 755 ./scripts/Scanning /home/$you/.config/caja/scripts/Scanning/Scan_For_Email
   for file in $(ls ./launchers)
     do
       sed "s/username/$you/" ./launchers/$file > /home/$you/.local/share/file-manager/actions/$file
